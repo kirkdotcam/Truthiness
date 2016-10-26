3a9b311d4f87582df70919f95a2ff2e74ab1fd9e
camkirk <camden.kirkland@gmail.com>
2016-10-26 02:27:19 -0500

    npm updated for express

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express=require("express");
 var app=express();
 
 
-app.get('/',function(){
-    express.static(__dirname+'index.html')
-    }).listen(process.env.PORT)
+app.get('/',function(req,res){
+    express.static(__dirname+'/index.html')
+    }).listen(process.env.PORT);
